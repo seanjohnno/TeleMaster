417a202befcdbca67d68f0727dc6dd1a84cc2949
Sean Johnson <s3anj@hotmail.co.uk>
2020-03-01 22:24:00 +0000

    Implementing limiting rent ordered items to 5

--- telemaster/mobile_phone_mast_queries.py
+++ telemaster/mobile_phone_mast_queries.py
@@ -6,4 +6,5 @@ class QueryByAscendingRent:
 
     def list_masts(self):
         all_masts = self.__mast_repository.list_all_masts()
-        return sorted(all_masts, key=mobile_phone_mast_repository.MobilePhoneMastInfo.rent)
+        ascending_rent_sorted_masts = sorted(all_masts, key=mobile_phone_mast_repository.MobilePhoneMastInfo.rent)
+        return ascending_rent_sorted_masts[:5]
