6a6a0e0d43006ff6efe2876691e56071e57d617e
Sean Johnson <s3anj@hotmail.co.uk>
2020-03-01 21:23:35 +0000

    Implementing repository to read csv data

--- telemaster/mobile_phone_mast_repository.py
+++ telemaster/mobile_phone_mast_repository.py
@@ -1,8 +1,15 @@
 import csv
+from typing import List
+
+class MobilePhoneMastInfo:
+    def __init__(self, csv_row):
+        self.__csv_row = csv_row
 
 class MobilePhoneMastRepository:
     def __init__(self, csv_file_location: str):
         self.__csv_file_location = csv_file_location
 
-    def list_all_masts(self):
-        return []
+    def list_all_masts(self) -> List[MobilePhoneMastInfo]:
+        with open(self.__csv_file_location) as csvfile:
+             reader = csv.DictReader(csvfile)
+             return [MobilePhoneMastInfo(row) for row in reader]
