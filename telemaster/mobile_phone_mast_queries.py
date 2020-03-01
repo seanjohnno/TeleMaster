d21f48812a42e30ba009a2bb4f2f3d52937be01b
Sean Johnson <s3anj@hotmail.co.uk>
2020-03-01 23:28:35 +0000

    Implementing query by lease dates

--- telemaster/mobile_phone_mast_queries.py
+++ telemaster/mobile_phone_mast_queries.py
@@ -1,4 +1,6 @@
 import collections
+import datetime
+import time
 from telemaster import mobile_phone_mast_repository
 from typing import Dict, List
 
@@ -35,8 +37,23 @@ class QueryTenantMastCounts:
 
 class QueryLeaseIsBetweenDates:
     
+    JUNE_1ST_1999 = datetime.date(1999, 6, 1)
+    AUGUST_31ST_2007 = datetime.date(2007, 8, 31)
+
     def __init__(self, mast_repository: mobile_phone_mast_repository.MobilePhoneMastRepository):
         self.__mast_repository = mast_repository
 
-    def list_masts(self) -> List[mobile_phone_mast_repository.MobilePhoneMastRepository]:
-        return []
+    def list_masts(self):
+        start_date = QueryLeaseIsBetweenDates.JUNE_1ST_1999
+        end_date = QueryLeaseIsBetweenDates.AUGUST_31ST_2007
+
+        all_masts = self.__mast_repository.list_all_masts()
+        return [mast for mast in all_masts if self.__mast_is_within_dates_inclusive(mast, start_date, end_date)]
+
+    def __mast_is_within_dates_inclusive(self, mast, start_date, end_date):
+        parsed_date_struct = time.strptime(mast.lease_start_date(), '%d %b %Y')
+        parsed_date = datetime.date(parsed_date_struct[0], parsed_date_struct[1], parsed_date_struct[2])
+
+        if parsed_date >= start_date and parsed_date <= end_date:
+            return True
+        return False
