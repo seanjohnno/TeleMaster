2dbd7191e594a5b31ebcd5ca7ea76edfb0a2e9a4
Sean Johnson <s3anj@hotmail.co.uk>
2020-03-01 22:24:55 +0000

    Refactor step, introducing constant for readability

--- telemaster/mobile_phone_mast_queries.py
+++ telemaster/mobile_phone_mast_queries.py
@@ -1,10 +1,13 @@
 from telemaster import mobile_phone_mast_repository
 
 class QueryByAscendingRent:
+
+    ITEM_LIMIT = 5
+
     def __init__(self, mast_repository: mobile_phone_mast_repository.MobilePhoneMastRepository):
         self.__mast_repository = mast_repository
 
     def list_masts(self):
         all_masts = self.__mast_repository.list_all_masts()
         ascending_rent_sorted_masts = sorted(all_masts, key=mobile_phone_mast_repository.MobilePhoneMastInfo.rent)
-        return ascending_rent_sorted_masts[:5]
+        return ascending_rent_sorted_masts[:QueryByAscendingRent.ITEM_LIMIT]
