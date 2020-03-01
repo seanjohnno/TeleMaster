e519d31e67d36bdcdfbb920aefe43d639b62337a
Sean Johnson <s3anj@hotmail.co.uk>
2020-03-01 21:26:08 +0000

    Test refactor, adding constant for readability

--- tests/test_mobile_phone_mast_repository.py
+++ tests/test_mobile_phone_mast_repository.py
@@ -4,13 +4,15 @@ from telemaster import mobile_phone_mast_repository
 
 class TestMastRepository(unittest.TestCase):
 
+    EXPECTED_ROW_COUNT = 42
+
     def test_expected_row_count_is_loaded(self):
         csv_file_location = self.__get_mobile_phone_mast_csv_path()
 
         repository = mobile_phone_mast_repository.MobilePhoneMastRepository(csv_file_location)
-        mobile_telephone_masts = repository.list_all_masts()
+        mobile_telephone_mast_list = repository.list_all_masts()
 
-        self.assertEqual(len(mobile_telephone_masts), 42)
+        self.assertEqual(len(mobile_telephone_mast_list), TestMastRepository.EXPECTED_ROW_COUNT)
 
     def __get_mobile_phone_mast_csv_path(self):
         root_dir = pathlib.Path(__file__).parent.parent
