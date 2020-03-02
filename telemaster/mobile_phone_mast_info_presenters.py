465e5170eee2e3504a9ece7e7bec31379db2fb51
Sean Johnson <s3anj@hotmail.co.uk>
2020-03-02 02:38:45 +0000

    Implementing date formatting when supplied to presenter

--- telemaster/mobile_phone_mast_info_presenters.py
+++ telemaster/mobile_phone_mast_info_presenters.py
@@ -1,4 +1,6 @@
 from abc import ABCMeta, abstractmethod
+import datetime
+import time
 from typing import List
 from telemaster import mobile_phone_mast_repository
 
@@ -12,11 +14,14 @@ class FullMastInfoPresenter(IMastInfoPresenter):
 
     def __init__(self, mast_info_items: List[mobile_phone_mast_repository.MobilePhoneMastInfo], *, date_format: str = None):
         self.__mast_info_items = mast_info_items
+        self.__date_format = date_format
 
     def output_mast_info_items(self) -> str:
         return '\n'.join(self.__mast_info_to_str(mast_info_item) for mast_info_item in self.__mast_info_items)
 
     def __mast_info_to_str(self, mast_info_item: mobile_phone_mast_repository.MobilePhoneMastInfo) -> str:
+        
+        
         return '\n'.join([
             f'[Property Name]: {self.__replace_empty_str_with_dash(mast_info_item.property_name())}',
             f'[Property Address 1]: {self.__replace_empty_str_with_dash(mast_info_item.property_1st_line_address())}',
@@ -25,14 +30,25 @@ class FullMastInfoPresenter(IMastInfoPresenter):
             f'[Property Address 4]: {self.__replace_empty_str_with_dash(mast_info_item.property_4th_line_address())}',
             f'[Unit Name]: {self.__replace_empty_str_with_dash(mast_info_item.property_unit_name())}',
             f'[Tenant Name]: {self.__replace_empty_str_with_dash(mast_info_item.tenant_name())}',
-            f'[Lease Start Date]: {self.__replace_empty_str_with_dash(mast_info_item.lease_start_date())}',
-            f'[Lease End Date]: {self.__replace_empty_str_with_dash(mast_info_item.lease_end_date())}',
+            f'[Lease Start Date]: {self.__format_date(mast_info_item.lease_start_date())}',
+            f'[Lease End Date]: {self.__format_date(mast_info_item.lease_end_date())}',
             f'[Lease Years]: {self.__replace_empty_str_with_dash(mast_info_item.lease_years())}',
             f'[Current Rent]: {self.__replace_empty_str_with_dash(mast_info_item.rent())}'
         ])
 
     def __replace_empty_str_with_dash(self, input) -> str:
         return input if input else '-'
+
+    def __format_date(self, strdate) -> str:
+        if self.__date_format:
+            parsed_date_struct = time.strptime(strdate, '%d %b %Y')
+            parsed_date = datetime.date(parsed_date_struct[0], parsed_date_struct[1], parsed_date_struct[2])
+            return parsed_date.strftime(self.__date_format)
+
+        return strdate
+        
+    def __formatted_lease_end(self) -> str:
+        pass
 
 class TalliedRentPesenterDecorator(IMastInfoPresenter):
 
